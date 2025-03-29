c494a1999950337a887505bbe0d7ab7d5550e28c
Divinity-dev <divine_asiriuwa@yahoo.com>
2025-03-29 20:33:06 +0100

    Setup project and add tailwindcss

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <p className='text-center text-orange-800'>hello world</p>
+    </>
+  )
+}
+
+export default App
